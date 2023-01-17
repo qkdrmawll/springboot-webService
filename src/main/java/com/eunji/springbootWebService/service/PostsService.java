49e72c03dd284644aa0e3bfa066178ca4b4fec50
Eunji Bang <eunji8106@naver.com>
2023-01-17 18:23:47 +0900

    Update, Delete 기능 구현

--- src/main/java/com/eunji/springbootWebService/service/PostsService.java
+++ src/main/java/com/eunji/springbootWebService/service/PostsService.java
@@ -2,12 +2,16 @@ package com.eunji.springbootWebService.service;
 
 import com.eunji.springbootWebService.domain.posts.Posts;
 import com.eunji.springbootWebService.domain.posts.PostsRepository;
+import com.eunji.springbootWebService.web.dto.PostsListResponseDto;
 import com.eunji.springbootWebService.web.dto.PostsResponseDto;
 import com.eunji.springbootWebService.web.dto.PostsSaveRequestDto;
 import com.eunji.springbootWebService.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,11 +32,24 @@ public class PostsService {
 
         return id;
     }
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id= "+id));
+        postsRepository.delete(posts);
+    }
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= "+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
